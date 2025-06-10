b0fac03bd5c6d2ae392fc4e7746b9086b394c2f8
ciugiu <corneliu.ciugurean@epita.fr>
2025-06-10 16:53:37 +0200

    chore: Refactor project structure by updating .gitignore, adding initialization script, and creating SQL schema with sample data inserts

---
+++ import/Initialize.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+wget https://datasets.imdbws.com/name.basics.tsv.gz -P import/
+wget https://datasets.imdbws.com/title.akas.tsv.gz -P import/
+wget https://datasets.imdbws.com/title.basics.tsv.gz -P import/
+wget https://datasets.imdbws.com/title.crew.tsv.gz -P import/
+wget https://datasets.imdbws.com/title.episode.tsv.gz -P import/
+wget https://datasets.imdbws.com/title.principals.tsv.gz -P import/
+wget https://datasets.imdbws.com/title.ratings.tsv.gz -P import/
